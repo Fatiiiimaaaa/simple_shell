4c9c11ba51815477747e0917c7c453e5eb3581ca
Fatiiiimaaaa <oumhamedfatima0@gmail.com>
2023-11-16 04:45:57 -0800

    Betty style

--- builtin.c
+++ builtin.c
@@ -1,19 +1,20 @@
 #include "shell.h"
 
 /**
- * _exit_shell - exits the shell
- * @info: Structure containing potential arguments.
- *
- * Return: Exits with a given exit status (0) if info->argv[0] != "exit"
+ * _myexit - exits the shell
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: exits with a given exit status
+ *         (0) if info.argv[0] != "exit"
  */
-int _exit_shell(info_t *info)
+int _myexit(info_t *info)
 {
-	int exit_check;
+	int exitcheck;
 
-	if (info->argv[1])  /* If there is an exit argument */
+	if (info->argv[1])  /* If there is an exit arguement */
 	{
-		exit_check = _erratoi(info->argv[1]);
-		if (exit_check == -1)
+		exitcheck = _erratoi(info->argv[1]);
+		if (exitcheck == -1)
 		{
 			info->status = 2;
 			print_error(info, "Illegal number: ");
@@ -29,44 +30,42 @@ int _exit_shell(info_t *info)
 }
 
 /**
- * _change_directory - changes the current directory of the process
- * @info: Structure containing potential arguments.
- *
- * Return: Always 0
+ * _mycd - changes the current directory of the process
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
  */
-int _change_directory(info_t *info)
+int _mycd(info_t *info)
 {
-	char *current_dir, *target_dir, buffer[1024];
+	char *s, *dir, buffer[1024];
 	int chdir_ret;
 
-	current_dir = getcwd(buffer, 1024);
-	if (!current_dir)
+	s = getcwd(buffer, 1024);
+	if (!s)
 		_puts("TODO: >>getcwd failure emsg here<<\n");
-
 	if (!info->argv[1])
 	{
-		target_dir = _getenv(info, "HOME=");
-		if (!target_dir)
+		dir = _getenv(info, "HOME=");
+		if (!dir)
 			chdir_ret = /* TODO: what should this be? */
-				chdir((target_dir = _getenv(info, "PWD=")) ? target_dir : "/");
+				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
 		else
-			chdir_ret = chdir(target_dir);
+			chdir_ret = chdir(dir);
 	}
 	else if (_strcmp(info->argv[1], "-") == 0)
 	{
 		if (!_getenv(info, "OLDPWD="))
 		{
-			_puts(current_dir);
+			_puts(s);
 			_putchar('\n');
 			return (1);
 		}
 		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
 		chdir_ret = /* TODO: what should this be? */
-			chdir((target_dir = _getenv(info, "OLDPWD=")) ? target_dir : "/");
+			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
 	}
 	else
 		chdir_ret = chdir(info->argv[1]);
-
 	if (chdir_ret == -1)
 	{
 		print_error(info, "can't cd to ");
@@ -81,12 +80,12 @@ int _change_directory(info_t *info)
 }
 
 /**
- * _show_help - displays help information
- * @info: Structure containing potential arguments.
- *
- * Return: Always 0
+ * _myhelp - changes the current directory of the process
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ *  Return: Always 0
  */
-int _show_help(info_t *info)
+int _myhelp(info_t *info)
 {
 	char **arg_array;
 
@@ -96,4 +95,3 @@ int _show_help(info_t *info)
 		_puts(*arg_array); /* temp att_unused workaround */
 	return (0);
 }
-
--- errors.c
+++ errors.c
@@ -1,86 +1,85 @@
 #include "shell.h"
 
 /**
- * error_puts - prints an error message string to stderr
+ *_eputs - prints an input string
  * @str: the string to be printed
  *
  * Return: Nothing
  */
-void error_puts(char *str)
+void _eputs(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    if (!str)
-        return;
-    while (str[i] != '\0')
-    {
-        error_putchar(str[i]);
-        i++;
-    }
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
 }
 
 /**
- * error_putchar - writes the character c to stderr
+ * _eputchar - writes the character c to stderr
  * @c: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int error_putchar(char c)
+int _eputchar(char c)
 {
-    static int i;
-    static char buf[WRITE_BUF_SIZE];
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
 
-    if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-    {
-        write(2, buf, i);
-        i = 0;
-    }
-    if (c != BUF_FLUSH)
-        buf[i++] = c;
-    return (1);
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
 }
 
 /**
- * putchar_to_fd - writes the character c to given file descriptor
+ * _putfd - writes the character c to given fd
  * @c: The character to print
- * @fd: The file descriptor to write to
+ * @fd: The filedescriptor to write to
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int putchar_to_fd(char c, int fd)
+int _putfd(char c, int fd)
 {
-    static int i;
-    static char buf[WRITE_BUF_SIZE];
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
 
-    if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-    {
-        write(fd, buf, i);
-        i = 0;
-    }
-    if (c != BUF_FLUSH)
-        buf[i++] = c;
-    return (1);
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
 }
 
 /**
- * puts_to_fd - prints an input string to the given file descriptor
+ *_putsfd - prints an input string
  * @str: the string to be printed
- * @fd: the file descriptor to write to
+ * @fd: the filedescriptor to write to
  *
- * Return: the number of characters put
+ * Return: the number of chars put
  */
-int puts_to_fd(char *str, int fd)
+int _putsfd(char *str, int fd)
 {
-    int i = 0;
+	int i = 0;
 
-    if (!str)
-        return (0);
-    while (*str)
-    {
-        i += putchar_to_fd(*str++, fd);
-    }
-    return (i);
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
 }
-
